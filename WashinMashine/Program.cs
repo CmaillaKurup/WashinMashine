be22d85a9b3bdcdec3ce78429e422d76913123b4
CmaillaKurup <cami670v@zbc.dk>
2020-08-28 11:57:54 +0200

    First Commit

---
+++ WashinMashine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WashinMashine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
